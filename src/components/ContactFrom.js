0191368af3a6926fc6e23e9d45865cdcf8f7691f
farhanmail <maill.farhanismail@gmail.com>
2021-11-03 12:32:42 +0700

    refactoring home form

--- src/components/ContactFrom.js
+++ src/components/ContactFrom.js
@@ -22,8 +22,16 @@ function ContactFrom() {
     const [address, setAddress] = useState("")
     const [city, setCity] = useState("")
     const[addContact] = useMutation(ADD_CONTACT, { update: updateCache });
+    const [validated, setValidated] = useState(false);
 
-    const submitTask = () => {
+    const submitTask = (event) => {
+        const form = event.currentTarget;
+        if (form.checkValidity() === false) {
+        event.preventDefault();
+        event.stopPropagation();
+        }
+
+        setValidated(true);
         addContact({ variables: { first_name, last_name, phone_number, address, city } });
         setFirstName("")
         setLastName("")
@@ -32,52 +40,92 @@ function ContactFrom() {
         setCity("")
     }
 
+
+
     return (
-        <div>
-            <Form>
+            <div>
+            <Form noValidate validated={validated} onSubmit={submitTask}>
                 <Row className="mb-3">
-                    <Form.Group as={Col} controlId="formGridEmail">
-                    <Form.Label>First Name</Form.Label>
-                    <Form.Control type="email" placeholder="" 
-                    onChange={(e) => setFirstName(e.target.value)}/>
+                    <Form.Group as={Col} controlId="formGridFirstName">
+                        <Form.Label>First Name</Form.Label>
+                        <Form.Control 
+                            type="text"
+                            placeholder="First Name"
+                            className="taskInput"
+                            value={first_name}
+                            required
+                            onChange={(e) => setFirstName(e.target.value)}
+                        />
+                        <Form.Control.Feedback type="invalid">
+                            Please input First Name.
+                        </Form.Control.Feedback>
                     </Form.Group>
 
-                    <Form.Group as={Col} controlId="formGridPassword">
-                    <Form.Label>Last Name</Form.Label>
-                    <Form.Control type="Last" placeholder="Password" 
-                    onChange={(e) => setLastName(e.target.value)}/>
+                    <Form.Group as={Col} controlId="formGridLastName">
+                        <Form.Label>Last Name</Form.Label>
+                        <Form.Control 
+                            type="text"
+                            placeholder="Last Name"
+                            className="taskInput"
+                            value={last_name}
+                            onChange={(e) => setLastName(e.target.value)}
+                        />
                     </Form.Group>
                 </Row>
 
-                <Form.Group className="mb-3" controlId="formGridAddress1">
+                <Form.Group className="mb-3" controlId="formGridPhoneNumber">
                     <Form.Label>Phone Number</Form.Label>
-                    <Form.Control placeholder="1234567" 
-                    onChange={(e) => setPhoneNumber(e.target.value)}/>
+                    <Form.Control 
+                        required
+                        type="number"
+                        placeholder="Phone Number"
+                        className="taskInput"
+                        value={phone_number}
+                        onChange={(e) => setPhoneNumber(e.target.value)}
+                    />
+                    <Form.Control.Feedback type="invalid">
+                        Please input Phone Number.
+                    </Form.Control.Feedback>
                 </Form.Group>
 
                 <Row className="mb-3">
                     <Form.Group as={Col} controlId="formGridCity">
-                    <Form.Label>City</Form.Label>
-                    <Form.Control 
-                    onChange={(e) => setCity(e.target.value)}/>
+                        <Form.Label>City</Form.Label>
+                        <Form.Control 
+                            type="text"
+                            placeholder="City"
+                            className="taskInput"
+                            value={city}
+                            required
+                            onChange={(e) => setCity(e.target.value)}
+                        />
+                        <Form.Control.Feedback type="invalid">
+                            Please input City.
+                        </Form.Control.Feedback>
                     </Form.Group>
 
-                    <Form.Group as={Col} controlId="formGridZip">
-                    <Form.Label>Address</Form.Label>
-                    <Form.Control 
-                    onChange={(e) => setAddress(e.target.value)}/>
+                    <Form.Group as={Col} controlId="formGridAddress">
+                        <Form.Label>Address</Form.Label>
+                        <Form.Control 
+                            type="text"
+                            placeholder="Address"
+                            className="taskInput"
+                            value={address}
+                            required
+                            onChange={(e) => setAddress(e.target.value)}
+                        />
+                        <Form.Control.Feedback type="invalid">
+                            Please input Address.
+                        </Form.Control.Feedback>
                     </Form.Group>
                 </Row>
 
-                <Form.Group className="mb-3" id="formGridCheckbox">
-                    <Form.Check type="checkbox" label="Check me out" />
-                </Form.Group>
-
-                <Button variant="primary" type="submit" onClick={submitTask}>
+                <Button variant="primary" type="submit">
                     Submit
                 </Button>
             </Form>
         </div>
+        
     )
 }
 
